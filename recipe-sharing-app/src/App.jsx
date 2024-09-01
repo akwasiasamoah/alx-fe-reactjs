d8eb09764acaa7f8a6a34e38792a4f156a4ae934
akwasiasamoah <akwasiemmanuel64@gmail.com>
2024-09-01 18:43:04 +0000

    updated app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,18 @@
-// src/App.jsx
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 import AddRecipeForm from "./components/AddRecipeForm";
-import SearchBar from "./components/SearchBar";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-      <SearchBar />
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
