aa4e23fc5f43229f6e993d5cd3fa4635ebf0492c
akwasiasamoah <akwasiemmanuel64@gmail.com>
2024-09-01 18:32:09 +0000

    added useNavigation

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,13 +1,15 @@
 // src/components/DeleteRecipeButton.jsx
-
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 import useRecipeStore from "../recipeStore";
 
 const DeleteRecipeButton = ({ recipeId }) => {
+  const navigate = useNavigate();
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
+    navigate("/"); // Redirect to home or another page after deletion
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
