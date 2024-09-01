d77f67ed85b116390ab9db19903ed549ecdd2fb1
akwasiasamoah <akwasiemmanuel64@gmail.com>
2024-09-01 17:21:40 +0000

    added isError

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,4 +1,5 @@
 // src/components/PostsComponent.jsx
+
 import { useQuery } from "react-query";
 
 const fetchPosts = async () => {
@@ -10,10 +11,13 @@ const fetchPosts = async () => {
 };
 
 const PostsComponent = () => {
-  const { data, error, isLoading, refetch } = useQuery("posts", fetchPosts);
+  const { data, error, isLoading, isError, refetch } = useQuery(
+    "posts",
+    fetchPosts
+  );
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (isError) return <div>Error: {error.message}</div>;
 
   return (
     <div>
