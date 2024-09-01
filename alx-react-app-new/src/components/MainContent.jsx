b8482cf67a04db4cd7be005fd1688795eac6c5f8
akwasiasamoah <akwasiemmanuel64@gmail.com>
2024-09-01 19:51:04 +0000

    added new project

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+const MainContent = () => (
+  <main
+    style={{ padding: "20px", backgroundColor: "#f4f4f4", textAlign: "center" }}
+  >
+    <p>I love to visit New York, Paris, and Tokyo.</p>
+  </main>
+);
+
+export default MainContent;
