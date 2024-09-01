d0824d7c413268e5ae33b08df8cbcd2f71239c35
akwasiasamoah <akwasiemmanuel64@gmail.com>
2024-09-01 20:02:00 +0000

    added Counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,13 +1,9 @@
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <Counter />
     </div>
   );
 }
